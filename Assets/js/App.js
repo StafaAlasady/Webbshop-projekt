b4d42d7894b257a7aab028c0a513c7d4888468dc
StafaAlasady <Mustafa.alasady@outlook.com>
2024-02-11 17:01:30 +0100

    First page finished
    the index html is finished.

--- Assets/js/App.js
+++ Assets/js/App.js
@@ -1,5 +1,5 @@
 const buttonOpen = document.querySelector('[data-toggle="open"]');
-const buttonClose = document.querySelector('[data-toggle="Close"]');
+const buttonClose = document.querySelector('[data-toggle="close"]');
 const overlayEl = document.querySelector('.nav_overlay');
 const menuEl = document.querySelector('.nav_menu');
 
@@ -11,5 +11,23 @@ const toggleMenu = () => {
 }
 
 buttonOpen.addEventListener('click', toggleMenu);
-buttonClose.addEventListener('click', toggleMenu);
+buttonClose.addEventListener('click',toggleMenu);
 overlayEl.addEventListener('click', toggleMenu);
+window.addEventListener('resize', () =>{
+    if(this.innerWidth >= 1024)
+        if (menuEl.classList.contains('active')) toggleMenu();
+});
+
+const sr = ScrollReveal({
+    distance: '25px',
+    duration: 1000,
+    easing: 'cubic-bezier(.68, -.55, .265, 1.55)'
+});
+
+sr.reveal('.showcase_text-group',{
+    origin:'top'
+});
+
+sr.reveal('showcase_image-group',{
+    origin:'bottom'
+});
